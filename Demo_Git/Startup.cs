91b5995598f22cf39fc00bb634d3ea528b2b311f
Pinal Bhadanee <pinalbhadanee@gmail.com>
2018-03-20 16:25:03 +0530

    First TIme

---
+++ Demo_Git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_Git.Startup))]
+namespace Demo_Git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
